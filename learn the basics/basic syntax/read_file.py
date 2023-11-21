eb42d373eda96d901f348d9359cbf6cfd570db0e
etel <etelworkshop@gmail.com>
2023-11-21 16:29:56 -0600

    Read File

---
+++ learn the basics/basic syntax/read_file.py
@@ -0,0 +1,8 @@
+# I making 3 things. 
+#   1. Creating a variable. 
+#   2. Opening a file. 
+#   3. Executing to read de file.
+f = open("textoprueba.txt", "r")
+
+# Printing the file content.
+print(f.read())
