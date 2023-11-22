3d46ed83ffa985e014871cc7299d9e2908fe34b8
etel <etelworkshop@gmail.com>
2023-11-22 15:48:38 -0600

    Print line by line one file

---
+++ learn the basics/basic syntax/line_by_line.py
@@ -0,0 +1,9 @@
+# Read the 5 first characters of the file.
+f = open("./textopruebalinebyline.txt", "r")
+
+"""This is a
+multiline docstring."""
+
+for x in f:
+    # Printing the file content. 
+    print(x)
